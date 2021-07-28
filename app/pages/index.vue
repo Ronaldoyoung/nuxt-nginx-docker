acb2cba88dc472c226dcb3df429c765f3cf4b9c5
ronaldoyoung <44238550+Ronaldoyoung@users.noreply.github.com>
2021-07-28 21:12:30 +0900

    Update Vue~~

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        app hi!!! please Jenkins Build Test
+        app hi!!! please Jenkins Build Test!!
       </h1>
       <div class="links">
         <a
